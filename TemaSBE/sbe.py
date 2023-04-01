32e382e2196036292132326aa209672d33a943bc
Dragos <57534351+Dragos101@users.noreply.github.com>
2023-04-01 16:54:16 +0300

    changes
    1. publication working
    2. subscription WIP

--- TemaSBE/sbe.py
+++ TemaSBE/sbe.py
@@ -1,10 +1,12 @@
 import publication as p
 import subscription as s
   
-publicatie = p.Publication(2,"Pascani", 12, 0.8, 3.5, "NE", "25.03.2023")
-subscriptie = s.Subscription("Pascani", 12, 0.8, 3.5, "NE", "25.03.2023")
-print(publicatie)
-print("\n", subscriptie)
+# subscriptie = s.Subscription("Pascani", 12, 0.8, 3.5, "NE", "25.03.2023")
+# print("\n", subscriptie)
 
-#exemplu pentru nerespectarea constrangerilor
-#p = s.Publication(101, "Iasi", -5, 0.5, 40, "N", "5.01.2023")
+# publicatie = p.Publication()
+# print(publicatie)
+
+subscriptii = s.Subscription().generate_objects(10)
+for x in subscriptii:
+    print(x, "\n")
--- TemaSBE/subscription.py
+++ TemaSBE/subscription.py
@@ -1,23 +1,36 @@
-#clasa pentru subscriptii
-class Subscription:
-    def __init__(self, city=None, temp=None, rain=None, wind=None, direction=None, date=None):
-        self.city = city
-        self.temp = temp
-        self.rain = rain
-        self.wind = wind
-        self.direction = direction
-        self.date = date
-        
-    def __str__(self):
-        return f"{{(city,=,{self.city});(temp,>=,{self.temp});(rain,{self.rain});(wind,<,{self.wind});(direction,{self.direction})}}"
+import random
 
-#constrangerile pentru subscriptii
-subscription_structure = {
-    "city": {"frequency": 0.9, "type": str},
-    "temp": {"frequency": 0.8, "type": int},
-    "rain": {"frequency": 0.7, "type": float},
-    "wind": {"frequency": 0.8, "type": int},
-    "direction": {"type": str},
-    "date": {"type": str}
-}
+class Subscription: 
+    subscription_frequency = {
+        "city": {"frequency": 0.9},
+        "temp": {"frequency": 0.8},
+        "rain": {"frequency": 0.7},
+        "wind": {"frequency": 0.8},
+        "direction": {"frequency": 0.8},
+        "date": {"frequency": 0.8}
+    }
 
+    subscription_structure = {
+        "city": {"type": str, 'values': ["Pascani", "Brasov", "Cluj"]},
+        "temp": {"type": int, 'values': (-10, 40)},
+        "rain": {"type": float, 'values': (0.0, 1.0)},
+        "wind": {"type": int, 'values': (0, 30)},
+        "direction": {"type": str, 'values': ["N", "S", "E", "W", "NE", "NW", "SE", "SW"]},
+        "date": {"type": str, 'values': ["1.01.2023", "2.01.2023", "3.01.2023", "4.01.2023"]}
+    }
+
+    def generate_objects(self, n):
+        objects = []
+        for i in range(n):
+            obj = {}
+            for key, value in self.subscription_structure.items():
+                if key in self.subscription_frequency and random.random() > self.subscription_frequency[key]['frequency']:
+                    continue
+                if value['type'] == str:
+                    obj[key] = random.choice(value['values'])
+                elif value['type'] == int:
+                    obj[key] = random.randint(value['values'][0], value['values'][1])
+                elif value['type'] == float:
+                    obj[key] = round(random.uniform(value['values'][0], value['values'][1]), 2)
+            objects.append(obj)
+        return objects
